b3de80b23d58211e23d2e5872652a0a9f696e35d
Azhar Iqbal <niazi7589@gmail.com>
2025-11-21 00:33:37 +0500

    changed

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 
-import { BrowserRouter, Route, Routes } from 'react-router-dom'
+import {  Route, Routes } from 'react-router-dom'
 import './App.css'
 import Home from './Pages/Home'
 import About from './Pages/About'
